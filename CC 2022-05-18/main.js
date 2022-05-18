0f6d27636c836336cf412aae90fd2c59875eb175
pjosh0523 <61854901+pjosh0523@users.noreply.github.com>
2022-05-18 04:43:04 -0400

    added CC 2022-05-18 to repo

---
+++ CC 2022-05-18/main.js
@@ -0,0 +1,8 @@
+/*Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.*/
+//use the tostring method and put in 2 as the radix because 2 represents binary
+
+function addBinary(a,b){
+    return (a+b).toString(2)
+  }
