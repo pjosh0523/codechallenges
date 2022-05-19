6461201cd6b1aac1ea34295ec7914cce7aeecd9f
pjosh0523 <61854901+pjosh0523@users.noreply.github.com>
2022-05-19 04:39:37 -0400

    added CC 2022-05-19 to repo

---
+++ CC 2022-05-19/main.js
@@ -0,0 +1,9 @@
+/*Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).*/
+/*solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false*/
+//use the endwith method to find if str ends with ending 
+
+function solution(str, ending) {
+    return str.endsWith(ending)
+}
+
