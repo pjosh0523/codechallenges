c7f66b1c360c100c3ec0bdb2f687bb0c2c34ec45
pjosh0523 <61854901+pjosh0523@users.noreply.github.com>
2022-05-30 04:17:20 -0400

    added CC 2022-05-30 to repo

---
+++ CC 2022-05-30/main.js
@@ -0,0 +1,31 @@
+/*Write a program to determine if the two given numbers are coprime. A pair of numbers are coprime if their greatest shared factor is 1.
+
+The inputs will always be two positive integers between 2 and 99.
+
+Examples
+20 and 27:
+
+Factors of 20: 1, 2, 4, 5, 10, 20
+Factors of 27: 1, 3, 9, 27
+Greatest shared factor: 1
+Result: 20 and 27 are coprime
+12 and 39:
+
+Factors of 12: 1, 2, 3, 4, 6, 12
+Factors of 39: 1, 3, 13, 39
+Greatest shared factor: 3
+Result: 12 and 39 are not coprimes*/
+//what are we asking? if the two numbers share a co factor and is it 1
+
+function isCoprime(x, y) {
+    const min = Math.min(x, y);
+    
+    for (let i = 2; i <= min; i++) {
+      if (x % i === 0 && y % i === 0) {
+        return false;
+      }
+    }
+    
+    
+    return true;
+  }
