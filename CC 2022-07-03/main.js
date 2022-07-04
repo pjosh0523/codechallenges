ff1e6cb619535bdc7332fe110dbc553933df31ae
pjosh0523 <61854901+pjosh0523@users.noreply.github.com>
2022-07-03 20:13:43 -0400

    added CC 022-07-03 to repo

---
+++ CC 2022-07-03/main.js
@@ -0,0 +1,23 @@
+/*Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"*/
+
+//use toFixed to get only 2 decimal points 
+//
+
+function SeriesSum(n)
+{ 
+    for(a=0,i=1;i<=n*3;i+=3) a+=1/i;
+      return a.toFixed(2);
+}
