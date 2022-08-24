f33b3c74fd3db5dc74a0fe10e4f9cd8c8d38986a
pjosh0523 <61854901+pjosh0523@users.noreply.github.com>
2022-08-23 22:53:06 -0400

    added CC 2022-08-23 to repo

---
+++ CC 2022-08-23/main.js
@@ -0,0 +1,9 @@
+/*Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".*/
+
+//use template literals to take the string and put it in the response
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+
+  console.log(greet('ryan'))
