4b3f4e9ef83d1c8565cba155ad93ba798cd01dbe
pjosh0523 <61854901+pjosh0523@users.noreply.github.com>
2022-10-03 20:03:40 -0400

    added CC 2022-10-03 to repo

---
+++ CC 2022-10-03/main.js
@@ -0,0 +1,50 @@
+/*Create a function that takes a string and separates it into a sequence of letters.
+
+The array will be formatted as so:
+
+[['J','L','L','M']
+,['u','i','i','a']
+,['s','v','f','n']
+,['t','e','e','']]
+The function should separate each word into individual letters, with the first word in the sentence having its letters in the 0th index of each 2nd dimension array, and so on.
+
+Shorter words will have an empty string in the place once the word has already been mapped out. (See the last element in the last part of the array.)
+
+Examples:
+
+sepStr("Just Live Life Man")
+// => [['J','L','L','M'],
+// => ['u','i','i','a'],
+// => ['s','v','f','n'],
+// => ['t','e','e','']]);
+
+sepStr("The Mitochondria is the powerhouse of the cell")
+// => [ [ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
+// => [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
+// => [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
+// => [ '', 'o', '', '', 'e', '', '', 'l' ],
+// => [ '', 'c', '', '', 'r', '', '', '' ],
+// => [ '', 'h', '', '', 'h', '', '', '' ],
+// => [ '', 'o', '', '', 'o', '', '', '' ],
+// => [ '', 'n', '', '', 'u', '', '', '' ],
+// => [ '', 'd', '', '', 's', '', '', '' ],
+// => [ '', 'r', '', '', 'e', '', '', '' ],
+// => [ '', 'i', '', '', '', '', '', '' ],
+// => [ '', 'a', '', '', '', '', '', '' ]]*/
+
+
+//
+
+function sepStr(arr) {
+    let wordsArrs = arr.split(' ').map(x=> x.split(''));
+    let maxLength = Math.max(...wordsArrs.map(x=> x.length));
+    let res = [];
+    for(let i=0; i < maxLength; i++){
+      res.push([]);
+     for(let j = 0; j < wordsArrs.length; j++){
+       res[i].push(wordsArrs[j][i] ? wordsArrs[j][i] : '');
+     }
+    }
+    return res;
+   }
+sepStr('Just Live Life Man')
