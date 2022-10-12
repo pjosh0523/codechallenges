3a3b3455fef7caf37d5c4a933d49aad02ce1e172
pjosh0523 <61854901+pjosh0523@users.noreply.github.com>
2022-10-12 03:10:30 -0400

    added CC 2-22-10-12 to repo

---
+++ CC 2022-10-12/main.js
@@ -0,0 +1,54 @@
+/*You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Examples:
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+Note
+consecutive strings : follow one after another without an interruption*/
+
+//P: we have for parameters our array of strings and then an interger represented as k 
+//R: return the first longest string consisting of k consecutive stirngs in the arry 
+/*E: strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".*/
+
+
+//P: take array and map through it and concenate the elements needed
+
+function longestConsec(strarr, k) {
+    let longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+      let tempArray = strarr.slice(i,i+k);
+      let tempStr = tempArray.join("");
+      if(tempStr.length > longest.length){
+        longest = tempStr;
+      }
+    }
+    return longest;
+}
