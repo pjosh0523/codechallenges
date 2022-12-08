2615dcbeb0acf75da9cd879a7f58f380ec73a229
pjosh0523 <61854901+pjosh0523@users.noreply.github.com>
2022-12-07 19:40:51 -0500

    added CC 2022-12-07 to repo

---
+++ CC 2022-12-07/main.js
@@ -0,0 +1,48 @@
+/*Given two Arrays in which values are the power of each soldier, return true if you survive the attack or false if you perish.
+
+CONDITIONS
+
+Each soldier attacks the opposing soldier in the same index of the array. The survivor is the number with the highest value.
+If the value is the same they both perish
+If one of the values is empty(different array lengths) the non-empty value soldier survives.
+To survive the defending side must have more survivors than the attacking side.
+In case there are the same number of survivors in both sides, the winner is the team with the highest initial attack power. If the total attack power of both sides is the same return true.
+The initial attack power is the sum of all the values in each array.
+EXAMPLES
+
+attackers=[ 1, 3, 5, 7 ]   defenders=[ 2, 4, 6, 8 ]  
+//0 survivors                4 survivors
+//return true
+
+
+attackers=[ 1, 3, 5, 7 ]   defenders=[ 2, 4 ]  
+//2 survivors  (16 damage)   2 survivors (6 damage)
+//return false
+
+attackers=[ 1, 3, 5, 7 ]   defenders=[ 2, 4, 0, 8 ]  
+//1 survivors                3 survivors 
+//return true*/
+
+
+function hasSurvived(attackers, defenders) {
+    let attack = 0;
+    let defend = 0;
+    let attackSum = 0;
+    let defendSum = 0;
+    let i = 0;
+    while (attackers[i] || defenders[i]) {
+      if (!attackers[i] || attackers[i] < defenders[i]) {
+        defend++;
+      } else if (!defenders[i] || attackers[i] > defenders[i]) {
+        attack++;
+      }
+      if (attackers[i]) {
+        attackSum += attackers[i];
+      }
+      if (defenders[i]) {
+        defendSum += defenders[i];
+      }
+      i++;
+    }
+    return defend === attack ? attackSum <= defendSum : defend > attack;
+  }
